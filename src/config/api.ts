0845db3fd81d04fc87a64a895a0311273833f8e6
htet shine <your.email@example.com>
2023-11-14 22:03:22 +0630

    update

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,4 @@
 // export const BACKEND_URL : string = 'http://localhost:8003/api';
-export const BACKEND_URL : string = 'https://novax-mm.com/api';
+// export const BACKEND_URL : string = 'https://novax-mm.com/api';
+export const BACKEND_URL : string = 'https://www.bossnetworks.vip/bossnetworks_api/public/api';
+
